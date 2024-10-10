524d7fe710a656fae18566a3883a4bb622e83d32
Diego Vasquez <dvasquez@unicef.org>
2024-10-10 18:01:34 +0200

    adjusted certificate text, added date

--- app/certificate/page.tsx
+++ app/certificate/page.tsx
@@ -77,11 +77,12 @@ function CertificateContent() {
                   </div>
                   <div>
                     <div className="text-center mt-10">
-                      <div className="text-lg ">Provided to:</div>
+                      <div className="text-lg ">UNICEF ICTD TAO CORE presents to</div>
                       <div className="text-4xl tracking-tight">{user.name}</div>
                       <div className="w-[400px] border-b-4 border-unicef"></div>
-                      <div className="mt-5 text-lg">For having attended to:</div>
+                      <div className="mt-5 text-lg">For engaging with us in our </div>
                       <div className="text-xl tracking-tighter font-semibold text-blue-950">{user.course}</div>
+                      <div className="text-lg "> which took place on August 27 - 29, 2024</div>
                       <div></div>
                     </div>
                     <div className="text-center mt-10">
